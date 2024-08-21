77630cc5ebfe57be10f34e13f8f933b5335c9df0
Arina Wang <156151328+arinaa77@users.noreply.github.com>
2024-08-20 17:52:24 -0700

    Time: 43 ms (5.85%) | Memory: 16.5 MB (26.00%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,23 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        n = len(nums)
+
+        i = 0
+        while i < n:
+            start = i
+            # Find the current range of consective nums
+            while i < n - 1 and nums[i + 1] == nums[i] + 1:
+                i += 1
+            
+            # Construct the string
+            s = str(nums[start])
+
+            # Check if there are consectives to append
+            if i > start:
+                s += '->' + str(nums[i])
+
+            result.append(s)
+            i += 1
+        
+        return result
